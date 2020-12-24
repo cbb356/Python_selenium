6d7151f668bf45a2deb67461a0c78091dcd5e494
cbb356 <cbb356@gmail.com>
2020-12-24 18:19:41 +0200

    Session Helper created

--- fixture/application.py
+++ fixture/application.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
+from fixture.session_helper import SessionHelper
+
 
 class Application:
 
     def __init__(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(60)
+        self.session = SessionHelper(self)
 
     def destructor(self):
         self.driver.quit()
@@ -12,17 +15,6 @@ class Application:
     def open_page(self):
         driver = self.driver
         driver.get("http://localhost/addressbook/")
-
-    def login(self, username, password):
-        driver = self.driver
-        self.open_page()
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys(username)
-        driver.find_element_by_name("pass").click()
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys(password)
-        driver.find_element_by_css_selector("input[type=\"submit\"]").click()
 
     def open_groups_page(self):
         driver = self.driver
@@ -53,6 +45,3 @@ class Application:
         driver = self.driver
         driver.find_element_by_link_text("groups").click()
 
-    def logout(self):
-        driver = self.driver
-        driver.find_element_by_link_text("Logout").click()
