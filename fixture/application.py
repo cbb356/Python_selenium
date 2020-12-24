acc60573bc627e878b6c003ee75b8a30d0b07b10
cbb356 <cbb356@gmail.com>
2020-12-24 18:30:15 +0200

    GroupHelper created

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from fixture.session_helper import SessionHelper
-
+from fixture.group_helper import GroupHelper
 
 class Application:
 
@@ -8,6 +8,7 @@ class Application:
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(60)
         self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
 
     def destructor(self):
         self.driver.quit()
@@ -15,33 +16,4 @@ class Application:
     def open_page(self):
         driver = self.driver
         driver.get("http://localhost/addressbook/")
-
-    def open_groups_page(self):
-        driver = self.driver
-        driver.find_element_by_link_text("groups").click()
-
-    def group_creation(self, group):
-        driver = self.driver
-        # open groups page
-        self.open_groups_page()
-        # init new group creation
-        driver.find_element_by_name("new").click()
-        # group creation
-        driver.find_element_by_name("group_name").click()
-        driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys(group.name)
-        driver.find_element_by_name("group_header").click()
-        driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys(group.header)
-        driver.find_element_by_name("group_footer").click()
-        driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        driver.find_element_by_name("submit").click()
-        # return to groups page
-        self.return_to_groups_page()
-
-    def return_to_groups_page(self):
-        driver = self.driver
-        driver.find_element_by_link_text("groups").click()
 
