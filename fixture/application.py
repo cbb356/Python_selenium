e5eaf8dbb273a88710b36d1da012f8e155f664ed
cbb356 <cbb356@gmail.com>
2021-01-20 18:22:15 +0200

    fixture modification (if-else)

--- fixture/application.py
+++ fixture/application.py
@@ -14,6 +14,13 @@ class Application:
         self.navigation = NavigationHelper(self)
         self.navigation.open_home_page()
 
+    def is_valid(self):
+        try:
+            self.driver.current_url
+            return True
+        except:
+            return False
+
     def destructor(self):
         self.driver.quit()
 
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,11 +2,7 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
-    app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(name="test_name_modified"))
-    app.session.logout()
 
 def test_modify_group_header(app):
-    app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(header="test_header_modified"))
-    app.session.logout()
