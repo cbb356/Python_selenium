1e3e138c886013e12594750f1d667f66f3f36473
cbb356 <cbb356@gmail.com>
2021-01-20 17:35:02 +0200

    test_modify_group added

--- fixture/group_helper.py
+++ fixture/group_helper.py
@@ -14,30 +14,49 @@ class GroupHelper:
         # init new group creation
         driver.find_element_by_name("new").click()
         # group creation
-        driver.find_element_by_name("group_name").click()
-        driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys(group.name)
-        driver.find_element_by_name("group_header").click()
-        driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys(group.header)
-        driver.find_element_by_name("group_footer").click()
-        driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         # submit group creation
         driver.find_element_by_name("submit").click()
         # return to groups page
         self.return_to_groups_page()
 
+    def fill_group_form(self, group):
+        driver = self.app.driver
+        self.change_group_field_value("group_name", group.name)
+        self.change_group_field_value("group_header", group.header)
+        self.change_group_field_value("group_footer", group.footer)
+
+    def change_group_field_value(self, field_name, field_value):
+        driver = self.app.driver
+        if field_value is not None:
+            driver.find_element_by_name(field_name).click()
+            driver.find_element_by_name(field_name).clear()
+            driver.find_element_by_name(field_name).send_keys(field_value)
+
     def delete_first_group(self):
         driver = self.app.driver
-        # open groups page
         self.open_groups_page()
-        # select first group
-        driver.find_element_by_name("selected[]").click()
+        self.select_first_group()
         # delete selected group
         driver.find_element_by_name("delete").click()
-        # return to groups page
         self.return_to_groups_page()
+
+    def select_first_group(self):
+        driver = self.app.driver
+        driver.find_element_by_name("selected[]").click()
+
+    def modify_first_group(self, group):
+        driver = self.app.driver
+        self.open_groups_page()
+        self.select_first_group()
+        # init group modification
+        driver.find_element_by_name("edit").click()
+        # change group attributes
+        self.fill_group_form(group)
+        # submit modification
+        driver.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
 
     def return_to_groups_page(self):
         driver = self.app.driver
--- test/test_delete_first_group.py
+++ test/test_delete_first_group.py
@@ -1,4 +1,4 @@
-def test_add_empty_group(app):
+def test_delete_first_group(app):
     app.session.login(username="admin", password="secret")
     app.group.delete_first_group()
     app.session.logout()
